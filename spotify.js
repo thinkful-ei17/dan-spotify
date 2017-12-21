21cda962d927aa8e7a176c3134326d5c5dbf6685
Dan Rosen <drosen84@gmail.com>
2017-12-21 11:12:18 -0600

    starter files

--- spotify.js
+++ spotify.js
@@ -1,32 +1,34 @@
+'use strict';
+
 const CLIENT_ID = 'eb6dfffda5534bb5996e872487be4321';
 
 const getFromApi = function (endpoint, query = {}) {
-  // You won't need to change anything in this function, but you will use this function 
-  // to make calls to Spotify's different API endpoints. Pay close attention to this 
-  // function's two parameters.
+    // You won't need to change anything in this function, but you will use this function 
+    // to make calls to Spotify's different API endpoints. Pay close attention to this 
+    // function's two parameters.
 
-  const url = new URL(`https://api.spotify.com/v1/${endpoint}`);
-  const headers = new Headers();
-  headers.set('Authorization', `Bearer ${localStorage.getItem('SPOTIFY_ACCESS_TOKEN')}`);
-  headers.set('Content-Type', 'application/json');
-  const requestObject = {
-    headers
-  };
+    const url = new URL(`https://api.spotify.com/v1/${endpoint}`);
+    const headers = new Headers();
+    headers.set('Authorization', `Bearer ${localStorage.getItem('SPOTIFY_ACCESS_TOKEN')}`);
+    headers.set('Content-Type', 'application/json');
+    const requestObject = {
+        headers
+    };
 
-  Object.keys(query).forEach(key => url.searchParams.append(key, query[key]));
-  return fetch(url, requestObject).then(function (response) {
-    if (!response.ok) {
-      return Promise.reject(response.statusText);
-    }
-    return response.json();
-  });
+    Object.keys(query).forEach(key => url.searchParams.append(key, query[key]));
+    return fetch(url, requestObject).then(function (response) {
+        if (!response.ok) {
+            return Promise.reject(response.statusText);
+        }
+        return response.json();
+    });
 };
 
 let artist;
 
 const getArtist = function (name) {
-  // Edit me!
-  // (Plan to call `getFromApi()` several times over the whole exercise from here!)
+    // Edit me!
+    // (Plan to call `getFromApi()` several times over the whole exercise from here!)
 };
 
 
@@ -38,17 +40,17 @@ const getArtist = function (name) {
 // TO REVIEW FOR THIS EXERCISE
 // =========================================================================================================
 const login = function () {
-  const AUTH_REQUEST_URL = 'https://accounts.spotify.com/authorize';
-  const REDIRECT_URI = 'http://localhost:8080/auth.html';
+    const AUTH_REQUEST_URL = 'https://accounts.spotify.com/authorize';
+    const REDIRECT_URI = 'http://localhost:8080/auth.html';
 
-  const query = new URLSearchParams();
-  query.set('client_id', CLIENT_ID);
-  query.set('response_type', 'token');
-  query.set('redirect_uri', REDIRECT_URI);
+    const query = new URLSearchParams();
+    query.set('client_id', CLIENT_ID);
+    query.set('response_type', 'token');
+    query.set('redirect_uri', REDIRECT_URI);
 
-  window.location = AUTH_REQUEST_URL + '?' + query.toString();
+    window.location = AUTH_REQUEST_URL + '?' + query.toString();
 };
 
 $(() => {
-  $('#login').click(login);
+    $('#login').click(login);
 });
